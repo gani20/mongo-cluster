70fa9a8a49168b9c35c9486eed82f00696fc3478
Nicholas Valbusa <squallstar@gmail.com>
2015-04-29 16:00:17 +0100

    tmp fix

--- mongodb-replica/recipes/default.rb
+++ mongodb-replica/recipes/default.rb
@@ -1,2 +1,7 @@
 include_recipe "mongodb::10gen_repo"
 include_recipe "mongodb::replicaset"
+
+# Temporary fix for https://github.com/edelight/chef-mongodb/issues/316
+file('/etc/mongod.conf') { action :delete }
+file('/etc/init.d/mongod') { action :delete }
+file('/etc/init/mongod.conf') { action :delete }
