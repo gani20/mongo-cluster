c30d5a94e752d45aa5f3a013be7aa999c4bdb914
Nicholas Valbusa <squallstar@gmail.com>
2015-04-29 16:15:23 +0100

    one more chef fix

--- mongodb-replica/recipes/default.rb
+++ mongodb-replica/recipes/default.rb
@@ -1,7 +1,4 @@
+node.override['mongodb']['default_init_name'] = 'mongod'
+
 include_recipe "mongodb::10gen_repo"
 include_recipe "mongodb::replicaset"
-
-# Temporary fix for https://github.com/edelight/chef-mongodb/issues/316
-file('/etc/mongod.conf') { action :delete }
-file('/etc/init.d/mongod') { action :delete }
-file('/etc/init/mongod.conf') { action :delete }
