0dbcc1ea151f1735e51664a980f08c9d8355b857
Nicholas Valbusa <squallstar@gmail.com>
2016-02-29 14:21:47 +0000

    ssl support added

--- mongodb-replica/recipes/default.rb
+++ mongodb-replica/recipes/default.rb
@@ -1,4 +1,8 @@
 node.override['mongodb']['default_init_name'] = 'mongod'
+node.override['mongodb']['config']['dbpath'] = '/mnt/mongo-data/'
+node.override['mongodb']['config']['keyFile'] = '/etc/mongodb.key'
+node.override['mongodb']['config']['sslMode'] = 'requireSSL'
+node.override['mongodb']['config']['sslPEMKeyFile'] = '/etc/ssl/mongodb/concat.pem'
 
 include_recipe "mongodb::mongodb_org_repo"
 include_recipe "mongodb::replicaset"
